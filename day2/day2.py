fb74e8fac0459e6f86b9aca7df207c4ba3e779ba
Kris Knigga <kris@knigga.com>
2016-12-02 19:33:26 -0600

    Day 2, part 2

--- day2/day2.py
+++ day2/day2.py
@@ -22,7 +22,37 @@ class Keypad(object):
                 if not (self.key == 1 or self.key == 4 or self.key == 7):
                         self.key = self.key - 1
 
-key = Keypad()
+class GoofyKeypad(object):
+        def __init__(self):
+                self.key = "5"
+
+        def up(self):
+		#print "Moving up from " + self.key
+                if not (self.key == "5" or self.key == "2" or self.key == "1" or self.key == "4" or self.key == "9"):
+                        if not (self.key == "3" or self.key == "D"):
+                                self.key = hex(int(str(self.key), 16) - 4)[2:].upper()
+                        else:
+                                self.key = hex(int(str(self.key), 16) - 2)[2:].upper()
+
+        def down(self):
+		#print "Moving down from " + self.key
+                if not (self.key == "5" or self.key == "A" or self.key == "D" or self.key == "C" or self.key == "9"):
+			if not (self.key == "1" or self.key == "B"):
+                        	self.key = hex(int(str(self.key), 16) + 4)[2:].upper()
+			else:
+				self.key = hex(int(str(self.key), 16) + 2)[2:].upper()
+
+        def right(self):
+		#print "Moving right from " + self.key
+                if not (self.key == "1" or self.key == "4" or self.key == "9" or self.key == "C" or self.key == "D"):
+                        self.key = hex(int(str(self.key), 16) + 1)[2:].upper()
+
+        def left(self):
+		#print "Moving left from " + self.key
+                if not (self.key == "1" or self.key == "2" or self.key == "5" or self.key == "A" or self.key == "D"):
+                        self.key = hex(int(str(self.key), 16) - 1)[2:].upper()
+
+key = GoofyKeypad()
 
 with open(sys.argv[1]) as f:
         for line in f:
@@ -36,4 +66,4 @@ with open(sys.argv[1]) as f:
                         elif move == "L":
                                 key.left()
 
-                print key.key
+                print hex(int(key.key, 16))[2:].upper()
