622353aacefb8f52228d5d1167bc26398e52e732
Kris Knigga <kris@knigga.com>
2016-12-07 12:06:17 -0600

    Update template with new debug settings

--- template.py
+++ template.py
@@ -4,7 +4,12 @@ import os, sys, argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input_file")
+parser.add_argument("-d", "--debug", action='count')
 args = parser.parse_args()
 
+def debug_print(string, level=1):
+	if args.debug >= level:
+		print >> sys.stderr, string
+
 with open(args.input_file) as f:
-        for line in f:
+	for line in [ line.strip() for line in f ]:
