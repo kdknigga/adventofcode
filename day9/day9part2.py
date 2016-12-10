063d7a2e10ae2952a5608a49228a5cdc17bb13de
Kris Knigga <kris@knigga.com>
2016-12-09 22:02:38 -0600

    Day 9, part 2 -- slow, but works!

--- day9/day9part2.py
+++ day9/day9part2.py
@@ -15,46 +15,50 @@ def debug_print(string, level=1):
 marker_re = re.compile("\(([0-9]+)x([0-9]+)\)")
 
 def decompress(string):
-	output_string = ""
-	cursor = 0
-	match_count = 0
-	while cursor < len(string):
+	string_len = 0
+	debug_print("Incoming string: %s" % (string, ), 2)
+
+	while string:
 		# Find next repetition marker
-		marker = marker_re.search(string, cursor)
+		marker = marker_re.search(string)
 
 		if marker:
-			debug_print("Found marker: " + marker.group(0) + " at position: " + str(marker.start()))
-			match_count += 1
+			debug_print("Found marker: " + marker.group(0))
 
-			# All text since the last expansion until the next
-			# marker should be copied verbatim to the output
-			# string.
-			output_string += string[cursor:marker.start()]
+			# Count characters before the marker and
+			# and then discard them along with the marker
+			debug_print("Found %d chars before the marker" % (len(string[:marker.start()]), ), 2)
+			string_len += len(string[:marker.start()])
+			string = string[marker.end():]
+			debug_print("string trimmed to \"%s\"" % (string, ), 2)
 
-			# Find repeated string, repeat it, update output string
-			repeated_string = string[marker.end():marker.end() + int(marker.group(1))]
+			# Find repeated string, repeat it, and trim it
+			repeated_string = string[:int(marker.group(1))]
 			times_repeated = int(marker.group(2))
+			debug_print("Repeating \"%s\" %d times" % (repeated_string, times_repeated))
 			repeating_string = repeated_string * times_repeated
-			output_string += repeating_string
+			string = string[len(repeated_string):]
+			debug_print("string trimmed to \"%s\"" % (string, ), 2)
 
-			# Move cursor past the marker and the repeated string
-			cursor = marker.end() + int(marker.group(1))
+			# the repeating string might have markers in it, so
+			# send it back through decompress()
+			debug_print("Sending \"%s\" back through decompress()" % (repeating_string, ), 2)
+			len_from_substring = decompress(repeating_string)
+			debug_print("Substring length of %d returned" % (len_from_substring, ), 2)
+			string_len += len_from_substring
 
 		else:
-			# Finding no marker means the rest of the text goes
-			# straight to the output string
-			output_string += string[cursor:]
-			cursor = len(string)
+			# Finding no marker means add the remaining len
+			# to string_len and discard
+			debug_print("Found %s (%d) at the end of the string" % (string, len(string)), 2)
+			string_len += len(string)
+			string = ""
 
-	print "Expanded string: " + output_string
-	print "Expanded string length: " + str(len(output_string))
-	print "Strings decompressed: " + str(match_count)
-	print ""
-	return output_string, match_count
+	debug_print("Sending %d up to caller" % (string_len, ), 2)
+	return string_len
 
 with open(args.input_file) as f:
 	for line in [ line.strip() for line in f ]:
-		decompressed_string, match_count = decompress(line)
+		string_len = decompress(line)
 
-		while match_count > 0:
-			decompressed_string, match_count = decompress(decompressed_string)
+print "Length: %d" % (string_len, )
